a008c876a151c7a64348f10eead59aba6abc6e77
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-12-21 23:18:58 +0300

    feat: добавить форму поиска

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,7 +2,25 @@ import './SearchForm.css';
 
 function SearchForm() {
   return (
-    <div className="searchForm">SearchForm</div>
+    <section className="form-container">
+      <form
+      className="form"
+      name="form"
+      id="form"
+      noValidate>
+        <input
+        className="form__input"
+        // value={ || ""}
+        // onChange={handleChange}
+        // onClick={onClickHandler}
+        // onBlur={onOnBlur}
+        type="text"
+        name="form-search"
+        id="form-search"
+        required
+        placeholder="Поиск"/>
+      </form>
+    </section>
   );
 }
 
